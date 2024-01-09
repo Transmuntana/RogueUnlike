56499a504093a7bd34f9b0a90728ba05e832d732
joan [itb] aguayo planell <joan.aguayo.7e6@itb.cat>
2024-01-09 20:39:04 +0100

    healthbar, finishing touches

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Awake()
-    {
-
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0) && (GameManager._instance.inventory[GameManager._instance.item - 1] > 0) && (GameManager._instance.item == 3) && (GameManager._instance.playerHP < GameManager._instance.playerMaxHP))
+        {
+            GameManager._instance.inventory[GameManager._instance.item - 1] -= 1;
+            GameManager._instance.playerHP += 10;
+            if (GameManager._instance.playerHP > GameManager._instance.playerMaxHP) GameManager._instance.playerHP = GameManager._instance.playerMaxHP;
+        }
+        if (Input.GetKey(KeyCode.Keypad1))
+        {
+            GameManager._instance.item = 1;
+        }
+        if (Input.GetKey(KeyCode.Keypad2))
+        {
+            GameManager._instance.item = 2;
+        }
+        if (Input.GetKey(KeyCode.Keypad3))
+        {
+            GameManager._instance.item = 3;
+        }
+        if (Input.GetKey(KeyCode.Keypad4))
+        {
+            GameManager._instance.item = 4;
+        }
+        if (Input.GetKey(KeyCode.Keypad5))
+        {
+            GameManager._instance.item = 5;
+        }
     }
 }
